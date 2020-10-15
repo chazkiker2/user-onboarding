3b6c75dc6c3f8f1e1c2d83af693d6a021a1e14cd
chaz-kiker <chaz-kiker@lambdastudents.com>
2020-10-15 13:48:31 -0500

    add terms field to user card

--- src/components/User.js
+++ src/components/User.js
@@ -5,13 +5,14 @@ const User = props => {
 	if (!userInfo) {
 		return <h3>Working on fetching your user details...</h3>
 	}
-	const { id, name, password, email, role } = userInfo;
+	const { id, name, password, email, role, terms } = userInfo;
 	return (
 		<div className="user container">
 			<h4>{`Name: ${name}`}</h4>
 			<p>Password: {password}</p>
 			<p>Email: {email}</p>
 			<p>Role: {role}</p>
+			<p>Terms: {terms ? "agree" : "disagree"}</p>
 			<p>{id}</p>
 		</div>
 	);
